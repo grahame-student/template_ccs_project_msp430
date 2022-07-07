52c24c26cd8e49b3a6bccf501245a4a1110f6a14
Grahame White <4whitg16@solent.ac.uk>
2022-07-07 15:13:59 +0100

    Added placeholder unit test file

---
+++ src/tests/main.cpp
@@ -0,0 +1,12 @@
+// This file should not require modification
+// It's just there to execute all test cases
+
+#include "gtest/gtest.h"
+
+
+int main(int argc, char **argv)
+
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
